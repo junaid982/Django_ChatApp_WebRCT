e1cb249cf8328d8165b0af7163fe88ea605bbdd4
Junaid Ansari <ansarijunaid957@gmail.com>
2023-05-12 17:57:54 +0530

    created project 1

--- VideoChat/video/consumers.py
+++ VideoChat/video/consumers.py
@@ -6,4 +6,28 @@ class VideoChat(AsyncJsonWebsocketConsumer):
         await self.connect()
 
     async def receive_json(self, content):
-        pass
+        if(content['command'] == 'join_room'):
+            await self.channel_layer.group_add(content["room"] , self.channel_name)
+
+        elif (content['command'] == 'join'):
+            await self.channel_layer.group_send(content['room'],{
+                'type':'join.message'
+            })
+
+        elif (content['command'] == 'offer'):
+            await self.channel_layer.group_send(content['room'],{
+                'type':'offer.message',
+                'offer':content['offer']
+            })
+
+    async def join_message(self , event):
+        await self.send_json({
+            'command':'join'
+        }) 
+
+
+    async def offer_message(self , event):
+        await self.send_json({
+            'command':'offer',
+            'offer':event['offer']
+        })
